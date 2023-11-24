b571be0d2068ec037bacd7173689d44bd900e41f
Hüseyin Yener <huseyin.yener@bestcloudfor.me>
2023-11-24 10:42:13 +0300

    Verison New

--- app/src/main/java/com/bcfm/androidwebapp/MainActivity.java
+++ app/src/main/java/com/bcfm/androidwebapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.bcfm.androidwebapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.webkit.WebView;
 import android.os.Bundle;
+import android.app.ProgressDialog;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webview = (WebView) findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("https://bestcloudfor.me");
+
+        final ProgressDialog progress = ProgressDialog.show(this, "Bestcloudforme", "Yükleniyor....", true);
+        progress.show();
+        webview.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Toast.makeText(getApplicationContext(), "Sayfa yüklendi", Toast.LENGTH_SHORT).show();
+                progress.dismiss();
+            }
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(getApplicationContext(), "Bir hata oluştu", Toast.LENGTH_SHORT).show();
+                progress.dismiss();
+            }
+        });
     }
 }
